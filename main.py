be911e23a86788bf78e3c986542ee0ebd1a7231f
femphetamine <siltsu0@gmail.com>
2026-05-13 16:30:23 +0300

    Bot/Server Secrets

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Plans 
+# Tämän hetkinen:
+# Automatisoi setuppi.
 # 1. Kun uus henkilö liityy, henkilö laittaa bottiin RP nimensä, miten pääsi servulle, ja kiinostaako jengi asiat?
 # Sitten Upseeristo valitsee kyllä tai joo liittyäkseen
 # 2. Vaihteleva RPC botille, vaihtuu joka minuutti
-
+import secrets
+token = secrets.token
 from typing import Optional
-
 import discord
 from discord import app_commands
 from discord.ext import commands
-
 import math
 import threading
 import time
@@ -21,11 +22,15 @@ intents.guilds = True
 intents.guild_messages = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")  
-    activity = discord.Game(name=f"Kehittelyssä")
+    print(f"Logged in as {client.user}")
+    await tree.sync(guild=discord.Object(id=id1409292359690092697))
+    activity = discord.Game(name=f"Tällä hetkellä kehittämässä 'setup' komentoa.")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
-client.run("MTUwMzc3NzIxNTYyMjQxODQ5NA.GEaT7J.bsdF_pfIRMbF-oa9cVSNBGE1x_HC9Lvd_exbvo")
+client.run(token)
