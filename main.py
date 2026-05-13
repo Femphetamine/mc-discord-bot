860fb46fd527c039f0a1c130ebb7833cbc15510e
femphetamine <siltsu0@gmail.com>
2026-05-13 16:05:29 +0300

    remake

--- main.py
+++ main.py
@@ -25,16 +25,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")  
-    activity = discord.Game(name=f"Kerho elää!", 
-    large_image="https://i.imgur.com/E8hOFp1.jpeg",
-    large_text = "Kerho Elää!",
-    details = "024",
-    state="Mä oon vieläki tässä pelissä, nii älä äiti itke")
+    activity = discord.Game(name=f"Kehittelyssä")
     await client.change_presence(status=discord.Status.online, activity=activity)
-
-# @client.command()
-# async def PohjaSetup(ctx): # Luo MC Kerhon Pohja, roolit ja kanavat.
-#     print(f"Pohjasetup aloitettu klo {time.strftime}")
-#     pass
 
 client.run("MTUwMzc3NzIxNTYyMjQxODQ5NA.GEaT7J.bsdF_pfIRMbF-oa9cVSNBGE1x_HC9Lvd_exbvo")
