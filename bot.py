4ecfd4b35fc261c00534180853b34d6a6af6aa0d
femphetamine <siltsu0@gmail.com>
2026-05-13 07:13:12 +0300

    asasdasas

--- bot.py
+++ bot.py
@@ -1,57 +1,40 @@
 # Plans 
-# 1. Kun uus henkilö liityy, pistää bottiin RP nimensä, miten pääsi servulle, ja kiinostaako jengi asiat?
+# 1. Kun uus henkilö liityy, henkilö laittaa bottiin RP nimensä, miten pääsi servulle, ja kiinostaako jengi asiat?
+# Sitten Upseeristo valitsee kyllä tai joo liittyäkseen
+# 2. Vaihteleva RPC botille, vaihtuu joka minuutti
 
 from typing import Optional
 
 import discord
 from discord import app_commands
+from discord.ext import commands
+
+import math
+import threading
+import time
+import asyncio
 
 intents = discord.Intents.default() # Mitä asioita botti haluaa nähdä?
 intents.message_content = True
 intents.members = True
 intents.guilds = True
+intents.guild_messages = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"We have logged in as {client.user}")  
+    activity = discord.Game(name=f"Kerho elää!", 
+    large_image="https://i.imgur.com/E8hOFp1.jpeg",
+    large_text = "Kerho Elää!",
+    details = "024",
+    state="Mä oon vieläki tässä pelissä, nii älä äiti itke")
+    await client.change_presence(status=discord.Status.online, activity=activity)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    # New Command
-    if message.content.startswith("!!##SetupSäännöt"):
-        await message.channel.send("--  Kunnioita hierarkiaa  --\n" \
-        "1. Presidentin sana on laki.\n"
-        "2. Varapresidentti johtaa presidentin poissa ollessa.\n"
-        "3. Toimi roolisi sisällä, ellei muuten sanottu.\n" \
-        "\n"
-        "-- Käyttäytyminen --\n"
-        "1. Kunnioita kaikkia jäseniä, jokainen jäsen on yhtä tärkeä. NAISIA EI LOUKATA/HAKATA.\n"
-        "2. Ei selän takana puhumista, luottamuss on kaiken perusta.\n" \
-        "3. Kilpailevien jengien tai MC:iden kanssa ei toimita ilman lupaa.\n" \
-        "4. Rikollisuutta ei tehdä liivit tai tunnistettavilla vaatteilla. vaihda ajoneuvot, vaatteet ja naamioidu ennen rikollisuutta. Autot hävitetään rikoksien jälkeen.\n" \
-        "\n" \
-        "-- Seuraukset --\n" \
-        "Sääntöjen jatkuva rikkominen seuraa erotus, ja päädyt tappo listalle.")
-    # New Command
-    if message.content.startswith("!!##SetupKysyJäsenyys"):
-        await message.channel.send("Kirjoita tähän RP nimesi, ja kenen kautta pääsit serverille.")
-    # New Command
-    if message.content.startswith("!!##SetupHierarkia"):
-        await message.channel.send("<@&1503768215514386575> -- Johtaa ja edustaa klubia, päätökset tehdään yhdessä upseeriston kanssa.\n" \
-        "\n" \
-        "<@&1503768703593087037> -- Presidentin tuki, osallistuu päätöksiin ja hoitaa asiat kun presidentti ei paikalla.\n" \
-        "\n" \
-        "<@&1503806572214751232> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503769082028232845> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503768975174144020> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503769417706766386> -- Ei kuvausta")
-
+@client.command()
+async def PohjaSetup(ctx): # Luo MC Kerhon Pohja, roolit ja kanavat.
+    print(f"Pohjasetup aloitettu klo {time.strftime}")
+    pass
 
 client.run("MTUwMzc3NzIxNTYyMjQxODQ5NA.GEaT7J.bsdF_pfIRMbF-oa9cVSNBGE1x_HC9Lvd_exbvo")
