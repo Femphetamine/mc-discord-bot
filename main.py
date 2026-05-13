2ad8f2f2d019170e7b09ee1b8ab6850c69f8e5e9
femphetamine <siltsu0@gmail.com>
2026-05-13 17:46:09 +0300

    security flaw fix? maybe?

--- main.py
+++ main.py
@@ -2,32 +2,35 @@
 # Tämän hetkinen:
 # Automatisoi setuppi.
 # Vaihteleva RPC botille, vaihtuu joka minuutti
-import secrets
-token = secrets.token
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
 from typing import Optional
 import discord
 import discord.ext
 from discord.ext import commands
+from discord import app_commands
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 
-bot = commands.bot(command_prefix="!", intents = discord.intents.all())
-
-@bot.tree.command(name="test")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Päivää sullekki")
+intents = discord.Intents.all
+client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
     try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} commands!")
+        synced = await tree.sync()
+        print("Synced tree")
     except Exception as e:
         print(e)
-
+    print(f"Logged in as {client.user}")
     activity = discord.Game(name=f"Tällä hetkellä kehittämässä 'setup' komentoa.")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
-client.run(token)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="test")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Olen hereillä! {interaction.user.mention}")
+client.run(token) # type: ignore
