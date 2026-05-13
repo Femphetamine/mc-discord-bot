a5ea9e0cc81078fe31120e9701aca59b4981ca05
Femhpetamine <siltsu0@gmail.com>
2026-05-13 12:03:22 +0300

    n

--- bot.py
+++ bot.py
@@ -3,13 +3,18 @@
 
 from typing import Optional
 
-import discord
+import discord.ext
+import time
 from discord import app_commands
+true = True
+false = False
+spam = false
 
 intents = discord.Intents.default() # Mitä asioita botti haluaa nähdä?
 intents.message_content = True
 intents.members = True
 intents.guilds = True
+
 
 client = discord.Client(intents=intents)
 
@@ -17,41 +22,17 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
-@client.event
+@client.event  
 async def on_message(message):
     if message.author == client.user:
         return
-    # New Command
-    if message.content.startswith("!!##SetupSäännöt"):
-        await message.channel.send("--  Kunnioita hierarkiaa  --\n" \
-        "1. Presidentin sana on laki.\n"
-        "2. Varapresidentti johtaa presidentin poissa ollessa.\n"
-        "3. Toimi roolisi sisällä, ellei muuten sanottu.\n" \
-        "\n"
-        "-- Käyttäytyminen --\n"
-        "1. Kunnioita kaikkia jäseniä, jokainen jäsen on yhtä tärkeä. NAISIA EI LOUKATA/HAKATA.\n"
-        "2. Ei selän takana puhumista, luottamuss on kaiken perusta.\n" \
-        "3. Kilpailevien jengien tai MC:iden kanssa ei toimita ilman lupaa.\n" \
-        "4. Rikollisuutta ei tehdä liivit tai tunnistettavilla vaatteilla. vaihda ajoneuvot, vaatteet ja naamioidu ennen rikollisuutta. Autot hävitetään rikoksien jälkeen.\n" \
-        "\n" \
-        "-- Seuraukset --\n" \
-        "Sääntöjen jatkuva rikkominen seuraa erotus, ja päädyt tappo listalle.")
-    # New Command
-    if message.content.startswith("!!##SetupKysyJäsenyys"):
-        await message.channel.send("Kirjoita tähän RP nimesi, ja kenen kautta pääsit serverille.")
-    # New Command
-    if message.content.startswith("!!##SetupHierarkia"):
-        await message.channel.send("<@&1503768215514386575> -- Johtaa ja edustaa klubia, päätökset tehdään yhdessä upseeriston kanssa.\n" \
-        "\n" \
-        "<@&1503768703593087037> -- Presidentin tuki, osallistuu päätöksiin ja hoitaa asiat kun presidentti ei paikalla.\n" \
-        "\n" \
-        "<@&1503806572214751232> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503769082028232845> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503768975174144020> -- Ei kuvausta\n" \
-        "\n" \
-        "<@&1503769417706766386> -- Ei kuvausta")
-
+    if message.content.startswith("test"):
+        print("Terminal test ping")
+        spam = true
+        while spam == true:
+            await message.channel.send("botti elää perkel")
+            time.sleep(2)
+            spam = false
+        return
 
 client.run("MTUwMzc3NzIxNTYyMjQxODQ5NA.GEaT7J.bsdF_pfIRMbF-oa9cVSNBGE1x_HC9Lvd_exbvo")
