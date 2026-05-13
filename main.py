264b4dd8f2f86caf8bd53aa151db5a6f8caf297e
femphetamine <siltsu0@gmail.com>
2026-05-13 16:43:44 +0300

    Building first command

--- main.py
+++ main.py
@@ -24,12 +24,16 @@ intents.guild_messages = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-@
+@tree.command(
+        name="MC Setup"
+        description="Botti rakentaa MC pohjan."
+        guild=discord.object(id=1409292359690092697)
+)
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
-    await tree.sync(guild=discord.Object(id=id1409292359690092697))
+    await tree.sync(guild=discord.Object(id=1409292359690092697))
     activity = discord.Game(name=f"Tällä hetkellä kehittämässä 'setup' komentoa.")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
