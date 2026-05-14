92452fe1a0225408375bc7dc34e41a48d7576b11
femphetamine <siltsu0@gmail.com>
2026-05-14 22:50:00 +0300

    Adding Commands

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# UUDELLEEN KOODAUS VITTU TÄÄ ON PERSEESTÄ
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -8,16 +7,15 @@ from typing import Optional
 import discord
 import discord.ext
 from discord.ext import commands
-from discord import Permissions, app_commands
+from discord import Permissions, app_commands # en tiiä tarviiko näitä, en haluu koskea.
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-# ephereal
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 @bot.command()
-async def päivitä_säännöt(ctx, *, arg, number):
+async def lähetä_säännöt(ctx, *, arg):
     if ctx.author == bot.user:
         return
     if ctx.author.id == 980559850234843177:
@@ -28,6 +26,51 @@ async def päivitä_säännöt(ctx, *, arg, number):
         print("Error")
         await ctx.send("Haista vittu!")
         return
+    
+@bot.command()
+async def lähetä_tiedotteet(ctx, *, arg):
+    if ctx.author == bot.user:
+        return
+    if ctx.author.id == 980559850234843177:
+        channel = discord.utils.get(ctx.guild.channels, name="tiedotteet")
+        print(f"!päivitä_säännöt käytettiin")
+        await channel.send(arg)
+    else:
+        print("Error")
+        await ctx.send("Haista vittu!")
+        return
+    
+@bot.command()
+async def lähetä_hierarkia(ctx, *, arg):
+    if ctx.author == bot.user:
+        return
+    if ctx.author.id == 980559850234843177:
+        channel = discord.utils.get(ctx.guild.channels, name="hierarkia")
+        print(f"!päivitä_säännöt käytettiin")
+        await channel.send(arg)
+    else:
+        print("Error")
+        await ctx.send("Haista vittu!")
+        return
+    
+@bot.command()
+async def lähetä_radio(ctx, *, arg):
+    if ctx.author == bot.user:
+        return
+    if ctx.author.id == 980559850234843177:
+        channel = discord.utils.get(ctx.guild.channels, name="radio")
+        print(f"!päivitä_säännöt käytettiin")
+        await channel.send(arg)
+    else:
+        print("Error")
+        await ctx.send("Haista vittu!")
+        return
+
+@bot.command()
+async def apua(ctx):
+    if ctx.author == bot.user:
+        return
+    await ctx.send("prefix = $  |   $lähetä_radio, $lähetä_hierarkia, $lähetä_tiedotteet, $apua")
 
 @bot.event
 async def on_ready():
