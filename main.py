fd734d4528ea25f3ca228e25e5680c734a97e134
femphetamine <siltsu0@gmail.com>
2026-05-14 23:06:39 +0300

    update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 async def lähetä_säännöt(ctx, *, arg):
     if ctx.author == bot.user:
         return
-    if ctx.author.id == 980559850234843177:
+    if ctx.author.id == 980559850234843177 or 684389100790349939:
         channel = discord.utils.get(ctx.guild.channels, name="säännöt")
         print(f"!päivitä_säännöt käytettiin")
         await channel.send(arg)
@@ -30,9 +30,9 @@ async def lähetä_säännöt(ctx, *, arg):
 async def lähetä_tiedotteet(ctx, *, arg):
     if ctx.author == bot.user:
         return
-    if ctx.author.id == 980559850234843177:
+    if ctx.author.id == 980559850234843177 or 684389100790349939:
         channel = discord.utils.get(ctx.guild.channels, name="tiedotteet")
-        print(f"!päivitä_säännöt käytettiin")
+        print(f"!lähetä_tiedotteet käytettiin")
         await channel.send(arg)
     else:
         await ctx.send("Haista vittu!")
@@ -42,9 +42,9 @@ async def lähetä_tiedotteet(ctx, *, arg):
 async def lähetä_hierarkia(ctx, *, arg):
     if ctx.author == bot.user:
         return
-    if ctx.author.id == 980559850234843177:
+    if ctx.author.id == 980559850234843177 or 684389100790349939:
         channel = discord.utils.get(ctx.guild.channels, name="hierarkia")
-        print(f"!päivitä_säännöt käytettiin")
+        print(f"!lähetä_hierarkia käytettiin")
         await channel.send(arg)
     else:
         await ctx.send("Haista vittu!")
@@ -54,24 +54,36 @@ async def lähetä_hierarkia(ctx, *, arg):
 async def lähetä_radio(ctx, *, arg):
     if ctx.author == bot.user:
         return
-    if ctx.author.id == 980559850234843177:
+    if ctx.author.id == 980559850234843177 or 684389100790349939:
         channel = discord.utils.get(ctx.guild.channels, name="radio")
-        print(f"!päivitä_säännöt käytettiin")
+        print(f"!lähetä_radio käytettiin")
         await channel.send(arg)
     else:
         await ctx.send("Haista vittu!")
         return
 
 @bot.command()
+async def lähetä_rooli(ctx, *, arg):
+    if ctx.author == bot.user:
+        return
+    if ctx.author.id == 980559850234843177 or 684389100790349939:
+        channel = discord.utils.get(ctx.guild.channels, name="roolit")
+        print(f"!päivitä_roolit käytettiin")
+        await channel.send(arg)
+    else:
+        await ctx.send("Haista vittu!")
+        return
+
+@bot.command() # Muista lisätä uusia tehtyjä komentoja!
 async def apua(ctx):
     if ctx.author == bot.user:
         return
-    await ctx.send("prefix = $  |   $lähetä_radio, $lähetä_hierarkia, $lähetä_tiedotteet, $apua")
+    await ctx.send("prefix = $  |   $lähetä_radio, $lähetä_hierarkia, $lähetä_tiedotteet, $lähetä_rooli $apua")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    activity = discord.Game(name=f"Kehittelyssä")
+    activity = discord.Game(name=f"Paikalla")
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
 bot.run(token) # type: ignores
